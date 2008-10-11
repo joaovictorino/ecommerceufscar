00bb029c40b0eddb50cdda257e75463adbf890dc
zejuniortdr <zejuniortdr@be1785ef-604d-0410-90b4-cd0e9fa19252>
2008-10-11 00:19:43 +0000

    commit da query solicitada pelo Mestre....

--- ecommerceufscar/ComercioEletronicoService/src/comercioeletronicoservice/Main.java
+++ ecommerceufscar/ComercioEletronicoService/src/comercioeletronicoservice/Main.java
@@ -4,6 +4,11 @@
  */
 package comercioeletronicoservice;
 
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.util.Date;
 import weka.associations.AprioriItemSet;
 import weka.core.FastVector;
 import weka.core.Instances;
@@ -36,8 +41,24 @@ public class Main {
             System.out.println("modelo");
             System.out.println(apriori.toString());
             System.out.println("teste");
+            
+            Connection db;
+            Statement sql;
+            Class.forName("org.postgresql.Driver");
+            db = DriverManager.getConnection("jdbc:postgresql:ecommerce", "postgre", "postgre");
+            sql = db.createStatement();
+            
+            String sqlText = "insert into grupo_interesse(data_criacao) values("+new Date()+")";
+            sql.executeUpdate(sqlText);
+            
+            String sqlTextCod = "select * from grupo_interesse order by oid desc limit 1 OFFSET 0 ";
+            ResultSet results = sql.executeQuery(sqlTextCod);
+            
+            
             for(int i=0; i < rules.length - 1; i++){
-                System.out.println(((AprioriItemSet)rules[0].elementAt(i)).toString(data) + " ==> " + ((AprioriItemSet)rules[1].elementAt(i)).toString(data));
+                String sqlGrpTemProd = "insert into grp_tem_produto(cod_grupo,cod_produto,codproximoproduto) values("+results.getString("cod_grupo")+","+((AprioriItemSet)rules[0].elementAt(i)).toString(data)+","+((AprioriItemSet)rules[1].elementAt(i)).toString(data)+")";
+                sql.executeUpdate(sqlGrpTemProd);
+                //System.out.println(((AprioriItemSet)rules[0].elementAt(i)).toString(data) + " ==> " + ((AprioriItemSet)rules[1].elementAt(i)).toString(data));
             }
         }catch(Exception ex){
             System.out.println(ex.getMessage());
